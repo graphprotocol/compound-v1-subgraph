b38730cdc2acc9cc2baf3d73daf0d488a5586b95
David Kajpust <kajpustd@gmail.com>
2019-01-03 15:35:55 -0500

    Manifest done, codegen works, ready to start mapping

---
+++ src/mappings/moneyMarket.ts
@@ -0,0 +1,57 @@
+import {BigInt} from '@graphprotocol/graph-ts'
+import {
+  BorrowLiquidated,
+  BorrowRepaid,
+  BorrowTaken,
+  NewOriginationFee,
+  NewRiskParameters,
+  SetMarketInterestRateModel,
+  SupplyReceived,
+  SupplyWithdrawn,
+  SupportedMarket,
+  SuspendedMarket
+} from '../types/MoneyMarket/MoneyMarket'
+
+import {
+
+} from '../types/schema'
+
+export function handleSupplyReceived(event: SupplyReceived): void {
+
+}
+
+export function handleSupplyWithdrawn(event: SupplyWithdrawn): void {
+
+}
+
+export function handleBorrowTaken(event: BorrowTaken): void {
+
+}
+
+export function handleBorrowRepaid(event: BorrowRepaid): void {
+
+}
+
+export function handleBorrowLiquidated(event: BorrowLiquidated): void {
+
+}
+
+export function handleSupportedMarket(event: SupportedMarket): void {
+
+}
+
+export function handleSuspendedMarket(event: SuspendedMarket): void {
+
+}
+
+export function handleNewRiskParameters(event: NewRiskParameters): void {
+
+}
+
+export function handleNewOriginationFee(event: NewOriginationFee): void {
+
+}
+
+export function handleSetMarketInterestRateModel(event: SetMarketInterestRateModel): void {
+
+}
